94b1a7dd1d39fe4247e05e07ccd5eda94e09a2f7
Chris Richardson <chris@bpi.cam.ac.uk>
2020-09-03 16:40:23 +0100

    Update test code

--- main.cpp
+++ main.cpp
@@ -7,25 +7,26 @@
 
 int main()
 {
-  int n = 2;
+  int n = 3;
 
   // Reference triangle
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> v(3, 2);
   v << -1.0, -1.0, -1.0, 1.0, 1.0, -1.0;
-  
+
+  // Evaluate polynomial at nodes, and get inverse
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> pt = create_lattice(n, v);
   Eigen::MatrixXd r = tabulate_triangle(n, pt).transpose();
   Eigen::MatrixXd w = r.inverse();
 
+  // Create a set of points for testing and evaluate at those points
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> pt2 = create_lattice(50, v);
-  Eigen::MatrixXd vals = tabulate_triangle(n, pt2).matrix();
+  auto vals = tabulate_triangle(n, pt2);
 
   for (int i = 0; i < pt2.rows(); ++i)
     {
-      Eigen::Matrix r = vals.row(i).transpose();
-      auto t2 = w * r;
-      std::cout << t2.rows() << "x" << t2.cols() << "\n";
-						    //    std::cout << std::setprecision(5) << std::fixed << std::setw(10) << pt2.row(i) << " " << w * (vals.row(i).transpose()).transpose() << "\n";
+      Eigen::Matrix<double, Eigen::Dynamic, 1>  r = vals.row(i).transpose();
+      auto t2 = (w * r).transpose();
+         std::cout << std::setprecision(5) << std::fixed << std::setw(10) << pt2.row(i) << " " << t2 << "\n";
     }
 
   

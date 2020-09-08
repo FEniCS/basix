654dd51d1a9968f8b82ee4d4b316bc2a1894e166
Chris Richardson <chris@bpi.cam.ac.uk>
2020-09-08 09:52:31 +0100

    Remove old files

--- lattice.h
+++
@@ -1,9 +0,0 @@
-#include <Eigen/Dense>
-
-#pragma once
-
-// Create a set of points on a line/triangle/tetrahedron in a grid
-Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
-  create_lattice(int n, const Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic,
-		 Eigen::RowMajor>& vertices);
-               

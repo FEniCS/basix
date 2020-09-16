97e34198e8cad22fb30fdfb0e85848adf86d5def
Chris Richardson <chris@bpi.cam.ac.uk>
2020-09-16 20:13:39 +0100

    Fix stupid bug in test

--- test/test_quadrature.py
+++ test/test_quadrature.py
@@ -5,9 +5,9 @@ import numpy as np
 
 @pytest.mark.parametrize("order", [1, 2, 3, 4, 6, 8, 10, 11])
 def test_quadrature(order):
+    b = 7.0
     h = 5.0
-    w = 7.0
-    simplex = [[0, 0], [h, 0], [0, w]]
+    simplex = [[0, 0], [b, 0], [0, h]]
     Qpts, Qwts = fiatx.make_quadrature(simplex, order)
     w = sum(Qwts)
-    assert np.isclose(w, 0.5 * w * h)
+    assert np.isclose(w, 0.5 * b * h)

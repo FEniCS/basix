3e39b7d6c912384dfb1680579206374dcd0cae5e
Chris Richardson <chris@bpi.cam.ac.uk>
2020-10-29 15:19:27 +0000

    Work on skbuild setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from skbuild import setup
+
+setup(
+    name="fenics-libtab",
+    version="0.0.1",
+    description="lib tab",
+    author="FEniCS Project",
+    license="MIT",
+    packages=["libtab"],
+    package_dir={"": "src"},
+    cmake_install_dir="src/libtab",
+)

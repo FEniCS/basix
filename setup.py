a8bd2daa86071f4675fa53db8f76b794e396dbd9
Chris Richardson <chris@bpi.cam.ac.uk>
2020-11-02 15:49:51 +0000

    Update name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from skbuild import setup
 setup(
     name="fenics-libtab",
     version="0.0.1",
-    description="lib tab",
+    description="FEniCS tabulation library",
     author="FEniCS Project",
     license="MIT",
     packages=["libtab"],

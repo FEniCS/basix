97f54675e69275d3260fd0005ba1633107e2a5f4
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-11-06 16:49:08 +0000

    doc

--- cpp/finite-element.h
+++ cpp/finite-element.h
@@ -97,17 +97,93 @@ public:
       const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic,
                           Eigen::RowMajor>& dualmat);
 
-  // Base permutations
+  /// Get the base permutations
+  /// The base permutations represent the effect of rotating or reflecting
+  /// a subentity of the cell on the numbering of the DOFs.
+  /// This returns a matrix with one row for each subentity permutation in
+  /// the following order:
+  ///   Reversing edge 0, reversing edge 1, ...
+  ///   Rotate face 0, reflect face 0, rotate face 1, reflect face 1, ...
+  ///
+  /// Example: Order 3 Lagrange on a triangle
+  /// ---------------------------------------
+  /// This space has 10 dofs arranged like:
+  /// 2
+  /// |\
+  /// 6 4
+  /// |  \
+  /// 5 9 3
+  /// |    \
+  /// 0-7-8-1
+  /// For this element, the base permutations are:
+  ///   [[0, 1, 2, 4, 3, 5, 6, 7, 8, 9],
+  ///    [0, 1, 2, 3, 4, 6, 5, 7, 8, 9]
+  ///    [0, 1, 2, 3, 4, 5, 6, 8, 7, 9]]
+  /// The first row shows the effect of reversing the diagonal edge. The
+  /// second row shows the effect of reversing the vertical edge. The third
+  /// row shows the effect of reversing the horizontal edge.
   Eigen::Array<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
   base_permutations() const
   {
     return _base_permutations;
   };
 
-  // Does the space contain vectors
+  /// Does the space contain vectors?
+  /// If true, the space contains vector-valued functions, so direction
+  /// correction will need to be applied.
   bool contains_vectors() const { return _contains_vectors; };
 
-  // Direction correction
+  /// Direction correction
+  /// The direction correction represents the effect of rotating or reflecting
+  /// a subentity of the cell on the direction of vector-values DOFs.
+  /// This returns a std::vector of matrices. Each matrix represents the effect
+  /// of a subentity permutation. Ther are contained in the std::vector in the
+  /// following order:
+  ///   Reversing edge 0, reversing edge 1, ...
+  ///   Rotate face 0, reflect face 0, rotate face 1, reflect face 1, ...
+  ///
+  /// Example: Order 1 Raviart-Thomas on a triangle
+  /// ---------------------------------------------
+  /// This space has 3 dofs arranged like:
+  ///   |\
+  ///   | \
+  ///   |  \
+  /// <-1   0
+  ///   |  / \
+  ///   | L ^ \
+  ///   |   |  \
+  ///    ---2---
+  /// These DOFs are integrals of normal components over the edges: DOFs 0 and 2
+  /// are oriented inward, DOF 1 is oriented outwards.
+  /// For this element, the direction correction matrices are:
+  ///   0: [[-1, 0, 0],
+  ///       [ 0, 1, 0],
+  ///       [ 0, 0, 1]]
+  ///   1: [[1,  0, 0],
+  ///       [0, -1, 0],
+  ///       [0,  0, 1]]
+  ///   2: [[1, 0,  0],
+  ///       [0, 1,  0],
+  ///       [0, 0, -1]]
+  /// The first matrix reverses DOF 0 (as this is on the first edge). The second
+  /// matrix reverses DOF 1 (as this is on the second edge). The third matrix
+  /// reverses DOF 2 (as this is on the third edge).
+  ///
+  /// Example: DOFs on the face of Order 2 Nedelec first kind on a tetrahedron
+  /// ------------------------------------------------------------------------
+  /// On a face of this tetrahedron, this space has two face tangent DOFs:
+  /// |\        |\
+  /// | \       | \
+  /// |  \      | ^\
+  /// |   \     | | \
+  /// | 0->\    | 1  \
+  /// |     \   |     \
+  ///  ------    ------
+  /// For these DOFs, the subblocks of the direction correction matrices are:
+  ///   rotation: [[-1, 1],
+  ///              [ 1, 0]]
+  ///   reflection: [[0, 1],
+  ///                [1, 0]]
   std::vector<
       Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>
   direction_correction() const

acf71232eb49f6ab87a021b626bf1a17312ef619
Chris Richardson <chris@bpi.cam.ac.uk>
2020-11-07 11:57:42 +0000

    Add doc string

--- cpp/defines.cpp
+++ cpp/defines.cpp
@@ -5,5 +5,8 @@
 
 namespace libtab
 {
+/// Return the version number of libtab
+/// to check compatibility if using across projects
+/// @return version string
 std::string version() { return str(LIBTAB_VERSION); }
 }; // namespace libtab

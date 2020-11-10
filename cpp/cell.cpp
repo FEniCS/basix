60f79336db2c9b128e0123b159e100fb9fd2b60a
Chris Richardson <chris@bpi.cam.ac.uk>
2020-11-10 13:13:12 +0000

    Adjust quad ordering

--- cpp/cell.cpp
+++ cpp/cell.cpp
@@ -79,7 +79,7 @@ std::vector<std::vector<std::vector<int>>> cell::topology(cell::Type celltype)
     // Vertices
     topo[0] = {{0}, {1}, {2}, {3}};
     // Edges
-    topo[1] = {{1, 2}, {2, 3}, {3, 0}, {0, 1}};
+    topo[1] = {{0, 2}, {2, 3}, {3, 1}, {1, 0}};
     // Cell
     topo[2] = {{0, 1, 2, 3}};
     break;

22b32c2f1c53745bfdd20c2fa532db9d222aae14
Chris Richardson <chris@bpi.cam.ac.uk>
2020-11-18 16:21:01 +0000

    Install global API header and method to find it (#58)
    * Add method to get include_path
    
    * Add test

---
+++ cpp/libtab.h
@@ -0,0 +1,18 @@
+// FIXME: just include everything for now
+// Need to define public API
+#include "cell.h"
+#include "crouzeix-raviart.h"
+#include "defines.h"
+#include "dof-permutations.h"
+#include "finite-element.h"
+#include "indexing.h"
+#include "integral-moments.h"
+#include "lagrange.h"
+#include "lattice.h"
+#include "nedelec.h"
+#include "nedelec-second-kind.h"
+#include "polynomial-set.h"
+#include "quadrature.h"
+#include "raviart-thomas.h"
+#include "regge.h"
+#include "util.h"
--- cpp/libtab/__init__.py
+++ cpp/libtab/__init__.py
@@ -1,2 +1,7 @@
 from .libtab import *
 from .libtab import __version__
+import os
+_include_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "include")
+
+def get_include_path():
+    return _include_path

a940194a3aaef1c54e8c7e17e63518d78478c5ac
Chris Richardson <chris@bpi.cam.ac.uk>
2020-11-23 10:04:09 +0000

    Add GLL points on pyramid

--- cpp/lattice.cpp
+++ cpp/lattice.cpp
@@ -214,12 +214,6 @@ Eigen::ArrayXXd lattice::create(cell::type celltype, int n,
   }
   case cell::type::pyramid:
   {
-    if (lattice_type == lattice::type::gll_warped)
-    {
-      throw std::runtime_error(
-          "Warped lattice not yet implemented for pyramid");
-    }
-
     if (n == 0)
     {
       Eigen::ArrayXXd x = Eigen::ArrayXXd::Constant(1, 3, 0.4);
@@ -228,6 +222,22 @@ Eigen::ArrayXXd lattice::create(cell::type celltype, int n,
     }
     else
     {
+      // Interpolate warp factor along interval
+      std::tuple<Eigen::ArrayXXd, Eigen::ArrayXd> pw
+          = quadrature::gauss_lobatto_legendre_line_rule(n + 1);
+      Eigen::ArrayXd pts = std::get<0>(pw) * 0.5;
+      for (int i = 0; i < n + 1; ++i)
+        pts[i] += (0.5 - static_cast<double>(i) / static_cast<double>(n));
+      FiniteElement L = create_dlagrange(cell::type::interval, n);
+
+      // Get interpolated value at r in range [-1, 1]
+      auto w = [&](double r) {
+        Eigen::ArrayXd rr = Eigen::ArrayXd::Constant(1, 0.5 * (r + 1.0));
+        Eigen::VectorXd v = L.tabulate(0, rr)[0];
+        double x = v.dot(pts.matrix());
+        return x;
+      };
+
       int b = (exterior == false) ? 1 : 0;
       n -= b * 3;
       int m = (n + 1) * (n + 2) * (2 * n + 3) / 6;
@@ -236,7 +246,93 @@ Eigen::ArrayXXd lattice::create(cell::type celltype, int n,
       for (int k = 0; k < n + 1; ++k)
         for (int j = 0; j < n + 1 - k; ++j)
           for (int i = 0; i < n + 1 - k; ++i)
-            points.row(c++) << h * (i + b), h * (j + b), h * (k + b);
+          {
+            double x = h * (i + b);
+            double y = h * (j + b);
+            double z = h * (k + b);
+
+            if (lattice_type == lattice::type::gll_warped)
+            {
+              // Barycentric coordinates of triangle in x-z plane
+              const double l1 = x;
+              const double l2 = z;
+              const double l3 = 1 - x - z;
+              // Barycentric coordinates of triangle in y-z plane
+              const double l4 = y;
+              const double l5 = z;
+              const double l6 = 1 - y - z;
+
+              // b1-b6 are the factors for each edge
+              double b1, f1, f2;
+              if (std::fabs(l1) < 1e-12)
+              {
+                b1 = 1.0;
+                f1 = 0.0;
+                f2 = 0.0;
+              }
+              else
+              {
+                b1 = 2.0 * l3 / (2.0 * l3 + l1) * 2.0 * l2 / (2.0 * l2 + l1);
+                f1 = l1 / (l1 + l4);
+                f2 = l1 / (l1 + l6);
+              }
+
+              const double r1 = (l2 - l3) * f1 + (l5 - l6) * (1 - f1);
+              const double r2 = (l2 - l3) * f2 + (l5 - l4) * (1 - f2);
+
+              double b2;
+              if (std::fabs(l2) < 1e-12)
+                b2 = 1.0;
+              else
+                b2 = 2.0 * l3 / (2.0 * l3 + l2) * 2.0 * l1 / (2.0 * l1 + l2);
+
+              double b3, f3, f4;
+              if (std::fabs(l3) < 1e-12)
+              {
+                b3 = 1.0;
+                f3 = 0.0;
+                f4 = 0.0;
+              }
+              else
+              {
+                b3 = 2.0 * l2 / (2.0 * l2 + l3) * 2.0 * l1 / (2.0 * l1 + l3);
+                f3 = l3 / (l3 + l4);
+                f4 = l3 / (l3 + l6);
+              }
+
+              const double r3 = (l2 - l1) * f3 + (l5 - l6) * (1.0 - f3);
+              const double r4 = (l2 - l1) * f4 + (l5 - l4) * (1.0 - f4);
+
+              double b4;
+              if (std::fabs(l4) < 1e-12)
+                b4 = 1.0;
+              else
+                b4 = 2 * l6 / (2.0 * l6 + l4) * 2.0 * l5 / (2.0 * l5 + l4);
+
+              double b5;
+              if (std::fabs(l5) < 1e-12)
+                b5 = 1.0;
+              else
+                b5 = 2.0 * l6 / (2.0 * l6 + l5) * 2.0 * l4 / (2.0 * l4 + l5);
+
+              double b6;
+              if (std::fabs(l6) < 1e-12)
+                b6 = 1.0;
+              else
+                b6 = 2.0 * l4 / (2.0 * l4 + l6) * 2.0 * l5 / (2.0 * l5 + l6);
+
+              double dx = -b3 * b4 * w(r3) - b3 * b6 * w(r4) + b2 * w(l1 - l3);
+              double dy = -b1 * b6 * w(r2) - b3 * b6 * w(r4) + b5 * w(l4 - l6);
+              double dz = b1 * b4 * w(r1) + b1 * b6 * w(r2) + b3 * b4 * w(r3)
+                          + b3 * b6 * w(r4);
+
+              x += dx;
+              y += dy;
+              z += dz;
+            }
+
+            points.row(c++) << x, y, z;
+          }
 
       return points;
     }

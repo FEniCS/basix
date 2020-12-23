ca6fcb54b8f0cd4bc0efab86adb8fd04b0996aae
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-12-23 12:09:41 +0000

    Added BDM elements

---
+++ cpp/brezzi-douglas-marini.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 Chris Richardson
+// FEniCS Project
+// SPDX-License-Identifier:    MIT
+
+#pragma once
+
+#include "finite-element.h"
+#include <string>
+
+namespace basix
+{
+/// Create BDM element
+/// @param celltype
+/// @param degree
+FiniteElement create_bdm(cell::type celltype, int degree,
+                         const std::string& name = std::string());
+
+} // namespace basix

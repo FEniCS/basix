3483cd3dc6aae24512e4653ee901491d5042e074
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-01-06 16:58:02 +0000

    Add tests that permutations have the correct multiplicative orders

--- test/test_permutations.py
+++ test/test_permutations.py
@@ -4,6 +4,7 @@
 
 import basix
 import pytest
+import numpy as np
 
 interval_elements = ["Lagrange", "Discontinuous Lagrange"]
 triangle_elements = [
@@ -25,46 +26,6 @@ def test_interval_permutation_size(element_name, order):
     assert len(e.base_permutations) == 0
 
 
-@pytest.mark.parametrize("element_name", triangle_elements)
-@pytest.mark.parametrize("order", [1, 2, 3])
-def test_triangle_permutation_size(element_name, order):
-    if element_name == "Crouzeix-Raviart" and order != 1:
-        pytest.xfail()
-
-    e = basix.create_element(element_name, "triangle", order)
-    assert len(e.base_permutations) == 3
-
-
-@pytest.mark.parametrize("element_name", tetrahedron_elements)
-@pytest.mark.parametrize("order", [1, 2, 3])
-def test_tetrahedron_permutation_size(element_name, order):
-    if element_name == "Crouzeix-Raviart" and order != 1:
-        pytest.xfail()
-
-    e = basix.create_element(element_name, "tetrahedron", order)
-    assert len(e.base_permutations) == 14
-
-
-@pytest.mark.parametrize("element_name", quadrilateral_elements)
-@pytest.mark.parametrize("order", [1, 2, 3])
-def test_quadrilateral_permutation_size(element_name, order):
-    if element_name == "Crouzeix-Raviart" and order != 1:
-        pytest.xfail()
-
-    e = basix.create_element(element_name, "quadrilateral", order)
-    assert len(e.base_permutations) == 4
-
-
-@pytest.mark.parametrize("element_name", hexahedron_elements)
-@pytest.mark.parametrize("order", [1, 2, 3])
-def test_hexahedron_permutation_size(element_name, order):
-    if element_name == "Crouzeix-Raviart" and order != 1:
-        pytest.xfail()
-
-    e = basix.create_element(element_name, "hexahedron", order)
-    assert len(e.base_permutations) == 24
-
-
 @pytest.mark.parametrize(
     "cell_name, element_name",
     [(cell, e) for cell, elements in [
@@ -84,3 +45,62 @@ def test_non_zero(cell_name, element_name, order):
     for perm in e.base_permutations:
         for row in perm:
             assert max(abs(i) for i in row) > 1e-6
+
+
+@pytest.mark.parametrize("element_name", triangle_elements)
+@pytest.mark.parametrize("order", [1, 2, 3])
+def test_triangle_permutation_orders(element_name, order):
+    if element_name == "Crouzeix-Raviart" and order != 1:
+        pytest.xfail()
+
+    e = basix.create_element(element_name, "triangle", order)
+    assert len(e.base_permutations) == 3
+
+    identity = np.identity(e.dim)
+    for i, order in enumerate([2, 2, 2]):
+        assert np.allclose(
+            np.linalg.matrix_power(e.base_permutations[i], order),
+            identity)
+
+
+@pytest.mark.parametrize("element_name", tetrahedron_elements)
+@pytest.mark.parametrize("order", [1, 2, 3])
+def test_tetrahedron_permutation_orders(element_name, order):
+    if element_name == "Crouzeix-Raviart" and order != 1:
+        pytest.xfail()
+
+    e = basix.create_element(element_name, "tetrahedron", order)
+    assert len(e.base_permutations) == 14
+
+    identity = np.identity(e.dim)
+    for i, order in enumerate([2, 2, 2, 2, 2, 2, 3, 2, 3, 2, 3, 2, 3, 2]):
+        assert np.allclose(
+            np.linalg.matrix_power(e.base_permutations[i], order),
+            identity)
+
+
+@pytest.mark.parametrize("element_name", quadrilateral_elements)
+@pytest.mark.parametrize("order", [1, 2, 3])
+def test_quadrilateral_permutation_orders(element_name, order):
+    e = basix.create_element(element_name, "quadrilateral", order)
+    assert len(e.base_permutations) == 4
+
+    identity = np.identity(e.dim)
+    for i, order in enumerate([2, 2, 2, 2]):
+        assert np.allclose(
+            np.linalg.matrix_power(e.base_permutations[i], order),
+            identity)
+
+
+@pytest.mark.parametrize("element_name", hexahedron_elements)
+@pytest.mark.parametrize("order", [1, 2, 3])
+def test_hexahedron_permutation_orders(element_name, order):
+    e = basix.create_element(element_name, "hexahedron", order)
+    assert len(e.base_permutations) == 24
+
+    identity = np.identity(e.dim)
+    for i, order in enumerate([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                               4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2]):
+        assert np.allclose(
+            np.linalg.matrix_power(e.base_permutations[i], order),
+            identity)

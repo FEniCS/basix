440a9cfb812853c78e3b32f2fe333d9a9cb1cce8
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-02-02 10:51:13 +0000

    add MappingType to __init__.py

--- cpp/basix/__init__.py
+++ cpp/basix/__init__.py
@@ -2,7 +2,7 @@ import os
 
 # Public interface
 from ._basixcpp import __version__
-from ._basixcpp import create_element, CellType, mapping_to_str, family_to_str
+from ._basixcpp import create_element, CellType, mapping_to_str, family_to_str, MappingType
 
 
 # To possibly be removed

37fc5e90ebe29b16c2ae70a57f3a0221548ee5dd
Garth N. Wells <gnw20@cam.ac.uk>
2021-03-06 21:41:49 +0000

    Re-factor cells

--- cpp/core/lattice.cpp
+++ cpp/core/lattice.cpp
@@ -268,165 +268,140 @@ xt::xtensor<double, 2> create_prism(int n, lattice::type lattice_type,
   return x;
 }
 //-----------------------------------------------------------------------------
-Eigen::ArrayXXd _create(cell::type celltype, int n, lattice::type lattice_type,
-                        bool exterior)
+xt::xtensor<double, 2> create_pyramid(int n, lattice::type lattice_type,
+                                      bool exterior)
 {
-  switch (celltype)
+  if (n == 0)
+    return {{0.4, 0.4, 0.2}};
+
+  const double h = 1.0 / static_cast<double>(n);
+
+  // Interpolate warp factor along interval
+  std::tuple<Eigen::ArrayXXd, Eigen::ArrayXd> pw
+      = quadrature::compute_gll_rule(n + 1);
+  Eigen::VectorXd pts = std::get<0>(pw) * 0.5;
+  pts *= 0.5;
+
+  for (int i = 0; i < n + 1; ++i)
+    pts[i] += (0.5 - static_cast<double>(i) / static_cast<double>(n));
+
+  // Get interpolated value at r in range [-1, 1]
+  FiniteElement L = create_dlagrange(cell::type::interval, n);
+  auto w = [&](double r) {
+    Eigen::ArrayXd rr = Eigen::ArrayXd::Constant(1, 0.5 * (r + 1.0));
+    Eigen::VectorXd v = L.tabulate(0, rr)[0].row(0);
+    return v.dot(pts);
+  };
+
+  const std::size_t b = exterior == false ? 1 : 0;
+  n -= b * 3;
+  std::size_t m = (n + 1) * (n + 2) * (2 * n + 3) / 6;
+  xt::xtensor<double, 2> points({m, 3});
+  int c = 0;
+  for (int k = 0; k < n + 1; ++k)
   {
-  case cell::type::prism:
-  {
-    if (n == 0)
+    for (int j = 0; j < n + 1 - k; ++j)
     {
-      Eigen::ArrayXXd x = Eigen::ArrayXXd::Constant(1, 3, 1.0 / 3.0);
-      x(0, 2) = 0.5;
-      return x;
-    }
+      for (int i = 0; i < n + 1 - k; ++i)
+      {
+        double x = h * (i + b);
+        double y = h * (j + b);
+        double z = h * (k + b);
 
-    const Eigen::ArrayXXd tri_pts
-        = _create(cell::type::triangle, n, lattice_type, exterior);
-    const Eigen::ArrayXXd line_pts
-        = _create(cell::type::interval, n, lattice_type, exterior);
+        if (lattice_type == lattice::type::gll_warped)
+        {
+          // Barycentric coordinates of triangle in x-z plane
+          const double l1 = x;
+          const double l2 = z;
+          const double l3 = 1 - x - z;
 
-    Eigen::ArrayX3d x(tri_pts.rows() * line_pts.rows(), 3);
-    x.leftCols(2) = tri_pts.replicate(line_pts.rows(), 1);
-    for (int i = 0; i < line_pts.rows(); ++i)
-      x.block(i * tri_pts.rows(), 2, tri_pts.rows(), 1) = line_pts(i, 0);
-    return x;
-  }
-  case cell::type::pyramid:
-  {
-    if (n == 0)
-    {
-      Eigen::ArrayXXd x = Eigen::ArrayXXd::Constant(1, 3, 0.4);
-      x(0, 2) = 0.2;
-      return x;
-    }
-    else
-    {
-      const double h = 1.0 / static_cast<double>(n);
+          // Barycentric coordinates of triangle in y-z plane
+          const double l4 = y;
+          const double l5 = z;
+          const double l6 = 1 - y - z;
 
-      // Interpolate warp factor along interval
-      std::tuple<Eigen::ArrayXXd, Eigen::ArrayXd> pw
-          = quadrature::compute_gll_rule(n + 1);
-      Eigen::VectorXd pts = std::get<0>(pw) * 0.5;
-      for (int i = 0; i < n + 1; ++i)
-        pts[i] += (0.5 - static_cast<double>(i) / static_cast<double>(n));
-      FiniteElement L = create_dlagrange(cell::type::interval, n);
-
-      // Get interpolated value at r in range [-1, 1]
-      auto w = [&](double r) {
-        Eigen::ArrayXd rr = Eigen::ArrayXd::Constant(1, 0.5 * (r + 1.0));
-        Eigen::VectorXd v = L.tabulate(0, rr)[0].row(0);
-        return v.dot(pts);
-      };
-
-      int b = (exterior == false) ? 1 : 0;
-      n -= b * 3;
-      int m = (n + 1) * (n + 2) * (2 * n + 3) / 6;
-      Eigen::ArrayX3d points(m, 3);
-      int c = 0;
-      for (int k = 0; k < n + 1; ++k)
-        for (int j = 0; j < n + 1 - k; ++j)
-          for (int i = 0; i < n + 1 - k; ++i)
+          // b1-b6 are the blending factors for each edge
+          double b1, f1, f2;
+          if (std::fabs(l1) < 1e-12)
           {
-            double x = h * (i + b);
-            double y = h * (j + b);
-            double z = h * (k + b);
-
-            if (lattice_type == lattice::type::gll_warped)
-            {
-              // Barycentric coordinates of triangle in x-z plane
-              const double l1 = x;
-              const double l2 = z;
-              const double l3 = 1 - x - z;
-              // Barycentric coordinates of triangle in y-z plane
-              const double l4 = y;
-              const double l5 = z;
-              const double l6 = 1 - y - z;
-
-              // b1-b6 are the blending factors for each edge
-              double b1, f1, f2;
-              if (std::fabs(l1) < 1e-12)
-              {
-                b1 = 1.0;
-                f1 = 0.0;
-                f2 = 0.0;
-              }
-              else
-              {
-                b1 = 2.0 * l3 / (2.0 * l3 + l1) * 2.0 * l2 / (2.0 * l2 + l1);
-                f1 = l1 / (l1 + l4);
-                f2 = l1 / (l1 + l6);
-              }
-
-              // r1-r4 are the edge positions for each of the z>0 edges
-              // calculated so that they use the barycentric coordinates
-              // of the triangle, if the point lies on a triangular face.
-              // f1-f4 are face selecting functions, which blend between
-              // adjacent triangular faces
-              const double r1 = (l2 - l3) * f1 + (l5 - l6) * (1 - f1);
-              const double r2 = (l2 - l3) * f2 + (l5 - l4) * (1 - f2);
-
-              double b2;
-              if (std::fabs(l2) < 1e-12)
-                b2 = 1.0;
-              else
-                b2 = 2.0 * l3 / (2.0 * l3 + l2) * 2.0 * l1 / (2.0 * l1 + l2);
-
-              double b3, f3, f4;
-              if (std::fabs(l3) < 1e-12)
-              {
-                b3 = 1.0;
-                f3 = 0.0;
-                f4 = 0.0;
-              }
-              else
-              {
-                b3 = 2.0 * l2 / (2.0 * l2 + l3) * 2.0 * l1 / (2.0 * l1 + l3);
-                f3 = l3 / (l3 + l4);
-                f4 = l3 / (l3 + l6);
-              }
-
-              const double r3 = (l2 - l1) * f3 + (l5 - l6) * (1.0 - f3);
-              const double r4 = (l2 - l1) * f4 + (l5 - l4) * (1.0 - f4);
-
-              double b4;
-              if (std::fabs(l4) < 1e-12)
-                b4 = 1.0;
-              else
-                b4 = 2 * l6 / (2.0 * l6 + l4) * 2.0 * l5 / (2.0 * l5 + l4);
-
-              double b5;
-              if (std::fabs(l5) < 1e-12)
-                b5 = 1.0;
-              else
-                b5 = 2.0 * l6 / (2.0 * l6 + l5) * 2.0 * l4 / (2.0 * l4 + l5);
-
-              double b6;
-              if (std::fabs(l6) < 1e-12)
-                b6 = 1.0;
-              else
-                b6 = 2.0 * l4 / (2.0 * l4 + l6) * 2.0 * l5 / (2.0 * l5 + l6);
-
-              double dx = -b3 * b4 * w(r3) - b3 * b6 * w(r4) + b2 * w(l1 - l3);
-              double dy = -b1 * b6 * w(r2) - b3 * b6 * w(r4) + b5 * w(l4 - l6);
-              double dz = b1 * b4 * w(r1) + b1 * b6 * w(r2) + b3 * b4 * w(r3)
-                          + b3 * b6 * w(r4);
-
-              x += dx;
-              y += dy;
-              z += dz;
-            }
-
-            points.row(c++) << x, y, z;
+            b1 = 1.0;
+            f1 = 0.0;
+            f2 = 0.0;
+          }
+          else
+          {
+            b1 = 2.0 * l3 / (2.0 * l3 + l1) * 2.0 * l2 / (2.0 * l2 + l1);
+            f1 = l1 / (l1 + l4);
+            f2 = l1 / (l1 + l6);
           }
 
-      return points;
+          // r1-r4 are the edge positions for each of the z>0 edges
+          // calculated so that they use the barycentric coordinates of
+          // the triangle, if the point lies on a triangular face. f1-f4
+          // are face selecting functions, which blend between adjacent
+          // triangular faces
+          const double r1 = (l2 - l3) * f1 + (l5 - l6) * (1 - f1);
+          const double r2 = (l2 - l3) * f2 + (l5 - l4) * (1 - f2);
+
+          double b2;
+          if (std::fabs(l2) < 1e-12)
+            b2 = 1.0;
+          else
+            b2 = 2.0 * l3 / (2.0 * l3 + l2) * 2.0 * l1 / (2.0 * l1 + l2);
+
+          double b3, f3, f4;
+          if (std::fabs(l3) < 1e-12)
+          {
+            b3 = 1.0;
+            f3 = 0.0;
+            f4 = 0.0;
+          }
+          else
+          {
+            b3 = 2.0 * l2 / (2.0 * l2 + l3) * 2.0 * l1 / (2.0 * l1 + l3);
+            f3 = l3 / (l3 + l4);
+            f4 = l3 / (l3 + l6);
+          }
+
+          const double r3 = (l2 - l1) * f3 + (l5 - l6) * (1.0 - f3);
+          const double r4 = (l2 - l1) * f4 + (l5 - l4) * (1.0 - f4);
+
+          double b4;
+          if (std::fabs(l4) < 1e-12)
+            b4 = 1.0;
+          else
+            b4 = 2 * l6 / (2.0 * l6 + l4) * 2.0 * l5 / (2.0 * l5 + l4);
+
+          double b5;
+          if (std::fabs(l5) < 1e-12)
+            b5 = 1.0;
+          else
+            b5 = 2.0 * l6 / (2.0 * l6 + l5) * 2.0 * l4 / (2.0 * l4 + l5);
+
+          double b6;
+          if (std::fabs(l6) < 1e-12)
+            b6 = 1.0;
+          else
+            b6 = 2.0 * l4 / (2.0 * l4 + l6) * 2.0 * l5 / (2.0 * l5 + l6);
+
+          double dx = -b3 * b4 * w(r3) - b3 * b6 * w(r4) + b2 * w(l1 - l3);
+          double dy = -b1 * b6 * w(r2) - b3 * b6 * w(r4) + b5 * w(l4 - l6);
+          double dz = b1 * b4 * w(r1) + b1 * b6 * w(r2) + b3 * b4 * w(r3)
+                      + b3 * b6 * w(r4);
+          x += dx;
+          y += dy;
+          z += dz;
+        }
+
+        points(c, 0) = x;
+        points(c, 1) = y;
+        points(c, 2) = z;
+        c++;
+      }
     }
   }
-  default:
-    throw std::runtime_error("Unsupported cell for lattice");
-  }
+
+  return points;
 }
 } // namespace
 //-----------------------------------------------------------------------------
@@ -453,31 +428,10 @@ xt::xtensor<double, 2> lattice::create(cell::type celltype, int n,
     return create_hex(n, type, exterior);
   case cell::type::prism:
     return create_prism(n, type, exterior);
+  case cell::type::pyramid:
+    return create_pyramid(n, type, exterior);
   default:
-    break;
+    throw std::runtime_error("Unsupported cell for lattice");
   }
-
-  // if (celltype == cell::type::triangle)
-  //   return create_tri(n, type, exterior);
-  // if (celltype == cell::type::quadrilateral)
-  // {
-  //   std::cout << "test (a): " << n << ", " << exterior << std::endl;
-  //   xt::xtensor<double, 2> tmp = create_quad(n, type, exterior);
-  //   std::cout << tmp << std::endl;
-  // }
-
-  Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> x
-      = _create(celltype, n, type, exterior);
-  std::vector<std::size_t> shape
-      = {(std::size_t)x.rows(), (std::size_t)x.cols()};
-
-  // if (celltype == cell::type::quadrilateral)
-  // {
-  //   std::cout << "test (b)" << std::endl;
-  //   std::cout << xt::adapt(x.data(), x.size(), xt::no_ownership(), shape)
-  //             << std::endl;
-  // }
-
-  return xt::adapt(x.data(), x.size(), xt::no_ownership(), shape);
 }
 //-----------------------------------------------------------------------------

1d718034e3524bf27530b92edc2d96a56165bee1
Garth N. Wells <gnw20@cam.ac.uk>
2021-04-04 14:13:35 +0100

    Update CR

--- cpp/basix/crouzeix-raviart.cpp
+++ cpp/basix/crouzeix-raviart.cpp
@@ -31,27 +31,29 @@ FiniteElement basix::create_cr(cell::type celltype, int degree)
   const xt::xtensor<double, 2> geometry = cell::geometry(celltype);
 
   const std::size_t ndofs = facet_topology.size();
-  xt::xtensor<double, 2> pts = xt::zeros<double>({ndofs, tdim});
-  xt::xtensor<double, 3> x
-      = xt::zeros<double>({ndofs, static_cast<std::size_t>(1), tdim});
+  // xt::xtensor<double, 2> pts = xt::zeros<double>({ndofs, tdim});
+  // xt::xtensor<double, 3> x
+  //     = xt::zeros<double>({ndofs, static_cast<std::size_t>(1), tdim});
+  std::array<std::vector<xt::xtensor<double, 3>>, 4> M;
+  std::array<std::vector<xt::xtensor<double, 2>>, 4> x;
+
+  x[tdim - 1] = std::vector<xt::xtensor<double, 2>>(
+      facet_topology.size(),
+      xt::zeros<double>({static_cast<std::size_t>(1), tdim}));
+
+  for (auto xe : x[tdim - 1])
+    std::cout << xe << std::endl;
 
   // Compute facet midpoints
-  int c = 0;
-  for (const std::vector<int>& f : facet_topology)
+  for (std::size_t f = 0; f < facet_topology.size(); ++f)
   {
-    for (int i : f)
-    {
-      for (std::size_t j = 0; j < geometry.shape(1); ++j)
-      {
-        pts(c, j) += geometry(i, j);
-        x(c, 0, j) += geometry(i, j);
-      }
-    }
+    // Loop over each vertex of the facet
+    for (int v : facet_topology[f])
+      x[tdim - 1][f] += xt::row(geometry, v);
 
-    for (std::size_t j = 0; j < geometry.shape(1); ++j)
-      pts(c, j) /= static_cast<double>(f.size());
-    xt::view(x, c, xt::all(), xt::all()) /= static_cast<double>(f.size());
-    ++c;
+    // Normalise
+    const std::size_t num_vertices = facet_topology[f].size();
+    x[tdim - 1][f] /= static_cast<double>(num_vertices);
   }
 
   std::size_t transform_count = tdim == 2 ? 3 : 14;
@@ -66,13 +68,15 @@ FiniteElement basix::create_cr(cell::type celltype, int degree)
   if (tdim == 3)
     entity_dofs[3] = {0};
 
-  xt::xtensor<double, 4> M({ndofs, 1, ndofs, 1});
-  xt::view(M, xt::all(), 0, xt::all(), 0) = xt::eye<double>(ndofs);
+  xt::xtensor<double, 3> Mf({1, 1, 1});
+  xt::view(Mf, xt::all(), 0, xt::all()) = xt::eye<double>(ndofs);
+  for (std::size_t i = 0; i < facet_topology.size(); ++i)
+    M[tdim - 1].push_back(Mf);
 
-  const xt::xtensor<double, 3> coeffs = compute_expansion_coefficients(
-      celltype, xt::eye<double>(ndofs), {M}, {x}, degree);
+  const xt::xtensor<double, 3> coeffs = compute_expansion_coefficients_new(
+      celltype, xt::eye<double>(ndofs), {M[tdim - 1]}, {x[tdim - 1]}, degree);
   return FiniteElement(element::family::CR, celltype, 1, {1}, coeffs,
-                       entity_dofs, base_transformations, pts,
-                       xt::eye<double>(ndofs), maps::type::identity);
+                       entity_dofs, base_transformations, x, M,
+                       maps::type::identity);
 }
 //-----------------------------------------------------------------------------

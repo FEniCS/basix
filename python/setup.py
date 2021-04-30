6af9b6b0a01c181d37be1b09e1760f15dc792e41
Jack S. Hale <mail@jackhale.co.uk>
2021-04-30 10:41:08 +0200

    Cleanup setup.py

--- python/setup.py
+++ python/setup.py
@@ -8,9 +8,26 @@ from distutils.version import LooseVersion
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-if sys.version_info < (3, 6):
-    print("Python 3.6 or higher required, please upgrade.")
+if sys.version_info < (3, 7):
+    print("Python 3.7 or higher required, please upgrade.")
     sys.exit(1)
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved :: MIT License
+Operating System :: POSIX
+Operating System :: POSIX :: Linux
+Operating System :: MacOS :: MacOS X
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Topic :: Scientific/Engineering :: Mathematics
+Topic :: Software Development :: Libraries :: Python Modules
+"""
 
 
 class CMakeExtension(Extension):
@@ -27,11 +44,6 @@ class CMakeBuild(build_ext):
             raise RuntimeError("CMake must be installed to build the following extensions: "
                                + ", ".join(e.name for e in self.extensions))
 
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
         for ext in self.extensions:
             self.build_extension(ext)
 
@@ -43,38 +55,31 @@ class CMakeBuild(build_ext):
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j3']
+        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+        build_args += ['--', '-j3']
 
         env = os.environ.copy()
-        import pybind11
-        print(env['PATH'])
-        env['pybind11_DIR'] = pybind11.get_cmake_dir()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
                                                               self.distribution.get_version())
-        # if not os.path.exists(self.build_temp):
-        #     os.makedirs(self.build_temp)
-        # subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        # subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp, env=env)
-        if not os.path.exists("./testing_build"):
-            os.makedirs("./testing_build")
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd="./testing_build", env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd="./testing_build", env=env)
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp, env=env)
 
 
 setup(name='fenics-basix',
       version='0.1.0',
       author='FEniCS Project',
-      description='BASIX Python interface',
+      author_email="fenics-dev@googlegroups.com",
+      maintainer_email="fenics-dev@googlegroups.com",
+      classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+      license="MIT license",
+      url="https://github.com/fenics/basix",
+      description='Basix Python interface',
       long_description='',
       packages=["basix"],
+      platforms=["Linux", "Mac OS-X", "Unix"],
       ext_modules=[CMakeExtension('basix._basixcpp')],
       cmdclass=dict(build_ext=CMakeBuild),
-      setup_requires=['pybind11'],
       zip_safe=False)

41a689aee634787b3369dff6de32a916a60bcf5a
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-06-22 08:47:32 +0100

    sort DOFs in set to get them in order

--- test/test_entity_dofs.py
+++ test/test_entity_dofs.py
@@ -31,10 +31,10 @@ def test_ordering_of_dofs(cell_name, order, element_name):
 
     entity_dofs = element.entity_dofs
     dof = 0
-
+    print(element.entity_dofs)
     for entity_dofs in element.entity_dofs:
         for dofs in entity_dofs:
-            for d in dofs:
+            for d in sorted(dofs):
                 assert d == dof
                 dof += 1
 

6435ae8a5235d5810bc364c53b7c60c156d12611
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-08-06 14:59:15 +0100

    docstrings

--- cpp/basix/lagrange.h
+++ cpp/basix/lagrange.h
@@ -11,25 +11,24 @@
 namespace basix
 {
 /// Create a Lagrange element on cell with given degree
-/// @param[in] celltype interval, triangle, quadrilateral, tetrahedral, or
-/// hexahedral celltype
-/// @param[in] degree
-/// @param[in] lattice_type
+/// @param[in] celltype The reference cell type that the element is defined on
+/// @param[in] degree The degree of the element
+/// @param[in] lattice_type The lattice type that should be used to arrange DOF
+/// points of the element
 /// @return A FiniteElement
 FiniteElement create_lagrange(cell::type celltype, int degree,
                               lattice::type lattice_type
                               = lattice::type::gll_warped);
 
 /// Create a Discontinuous Lagrange element on cell with given degree
-/// @param[in] celltype interval, triangle, quadrilateral, tetrahedral, or
-/// hexahedral celltype
-/// @param[in] degree
+/// @param[in] celltype The reference cell type that the element is defined on
+/// @param[in] degree The degree of the element
 /// @return A FiniteElement
 FiniteElement create_dlagrange(cell::type celltype, int degree);
 
 /// Create a DPC element on cell with given degree
-/// @param[in] celltype interval, quadrilateral or hexahedral celltype
-/// @param[in] degree
+/// @param[in] celltype The reference cell type that the element is defined on
+/// @param[in] degree The degree of the element
 /// @return A FiniteElement
 FiniteElement create_dpc(cell::type celltype, int degree);
 } // namespace basix

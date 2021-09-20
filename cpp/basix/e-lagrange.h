0d4298344def453b21585fbb424dfa507029a591
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-09-20 09:10:52 +0100

    docs

--- cpp/basix/e-lagrange.h
+++ cpp/basix/e-lagrange.h
@@ -14,7 +14,7 @@ namespace basix
 /// Create a Lagrange element on cell with given degree
 /// @param[in] celltype The reference cell type that the element is defined on
 /// @param[in] degree The degree of the element
-/// @param[in] lattice_type The lattice type that should be used to arrange DOFs
+/// @param[in] variant The variant of the element to be created
 /// @param[in] simplex_method The simplex method that should be used to create
 /// lattice on simplices
 /// @param discontinuous

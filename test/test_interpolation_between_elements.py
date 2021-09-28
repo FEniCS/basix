74220bb1b79c638136e73cbd11b4dd5735d1eb74
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-09-28 16:45:47 +0100

    flake

--- test/test_interpolation_between_elements.py
+++ test/test_interpolation_between_elements.py
@@ -5,7 +5,6 @@
 import numpy as np
 import basix
 import pytest
-from .utils import parametrize_over_elements
 
 
 def run_test(lower_element, higher_element, power, value_size):
@@ -63,8 +62,6 @@ def test_different_variant_interpolation(cell_type, order, variant1, variant2):
                                        basix.CellType.quadrilateral, basix.CellType.hexahedron])
 @pytest.mark.parametrize("orders", [(1, 2), (1, 3), (2, 3), (2, 4), (3, 4)])
 def test_different_order_interpolation_vector(family, args, cell_type, orders):
-    tdim = len(basix.topology(cell_type)) - 1
-
     lower_element = basix.create_element(family, cell_type, orders[0], *args)
     higher_element = basix.create_element(family, cell_type, orders[1], *args)
 
@@ -75,8 +72,6 @@ def test_different_order_interpolation_vector(family, args, cell_type, orders):
 @pytest.mark.parametrize("cell_type", [basix.CellType.triangle, basix.CellType.tetrahedron])
 @pytest.mark.parametrize("orders", [(1, 2), (1, 3), (2, 3), (2, 4), (3, 4)])
 def test_different_order_interpolation_matrix(family, args, cell_type, orders):
-    tdim = len(basix.topology(cell_type)) - 1
-
     lower_element = basix.create_element(family, cell_type, orders[0], *args)
     higher_element = basix.create_element(family, cell_type, orders[1], *args)
 
@@ -99,9 +94,7 @@ def test_different_order_interpolation_matrix(family, args, cell_type, orders):
                                        basix.CellType.quadrilateral, basix.CellType.hexahedron])
 @pytest.mark.parametrize("order", [1, 2, 3])
 def test_different_element_interpolation(family1, args1, family2, args2, cell_type, order):
-    tdim = len(basix.topology(cell_type)) - 1
-
     lower_element = basix.create_element(family1, cell_type, order, *args1)
     higher_element = basix.create_element(family2, cell_type, order, *args2)
 
-    run_test(lower_element, higher_element, order - 1, tdim)
+    run_test(lower_element, higher_element, order - 1, lower_element.value_size)

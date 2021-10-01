ce2825f389220588e46e5ab9c8401ff844cf9571
Garth N. Wells <gnw20@cam.ac.uk>
2021-10-01 15:21:14 +0100

    Update doc

--- cpp/basix/interpolation.h
+++ cpp/basix/interpolation.h
@@ -17,9 +17,10 @@ class FiniteElement;
 /// the interpolation between them. If one of the elements has value
 /// size 1, and the other has value size > 1, this function returns the
 /// interpolation matrix that maps the components of the element with a
-/// value size to/from the other element. If both element have different
-/// value sizes greater than 1, then this function throws a runtime
-/// error.
+/// value size to/from the other element.
+///
+/// @note If the elements have different value sizes and both are
+/// greater than 1, this function throws a runtime error
 ///
 /// @param[in] element_from The element to interpolate from
 /// @param[in] element_to The element to interpolate to

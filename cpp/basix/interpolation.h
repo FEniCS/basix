cc2ef5ab314fda68b4ed907f73576fcd5a0c22e6
Garth N. Wells <gnw20@cam.ac.uk>
2021-10-01 15:01:04 +0100

    Line breaking

--- cpp/basix/interpolation.h
+++ cpp/basix/interpolation.h
@@ -10,14 +10,16 @@
 namespace basix
 {
 
-/// Computes a matrix that represents the interpolation between two elements.
+/// Computes a matrix that represents the interpolation between two
+/// elements.
 ///
-/// If the two elements have the same value size, this function returns the
-/// interpolation between them. If one of the elements has value size 1, and the
-/// other has value size > 1, this function returns the interpolation matrix
-/// that maps the components of the element with a value size to/from the other
-/// element. If both element have different value sizes greater than 1, then
-/// this function throws a runtime error.
+/// If the two elements have the same value size, this function returns
+/// the interpolation between them. If one of the elements has value
+/// size 1, and the other has value size > 1, this function returns the
+/// interpolation matrix that maps the components of the element with a
+/// value size to/from the other element. If both element have different
+/// value sizes greater than 1, then this function throws a runtime
+/// error.
 ///
 /// @param[in] element_from The element to interpolate from
 /// @param[in] element_to The element to interpolate to

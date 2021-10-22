6b000dfd3cbedf7f9ffa656ac66b45322a9c6e00
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-10-22 08:00:10 +0100

    skip test if source not present

--- test/test_docs.py
+++ test/test_docs.py
@@ -1,9 +1,13 @@
 import os
+import pytest
 import sys
 
 
 def test_generated_docs():
     path = os.path.dirname(os.path.realpath(__file__))
+    if not os.path.isfile(os.path.join(path, "../python/docs.h")):
+        pytest.skip("This test can only be run from the source directory.")
+
     with open(os.path.join(path, "../python/docs.h")) as f:
         docs = f.read()
 

bcb1307251bbdfd9f5bf7284fd680018b12f4adb
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-10-26 10:31:44 +0100

    remove assert from demo

--- demo/demo_create_and_tabulate.py
+++ demo/demo_create_and_tabulate.py
@@ -45,5 +45,3 @@ print(tab.shape)
 # had asked for derivatives too. The second dimension (5) is the number of points.
 # The third dimension (25) is the number of DOFs. The fourth dimension (1) is the
 # value size of the element: this will be greater than 1 for vector-values elements.
-
-assert tab.shape == (1, 5, 25, 1)

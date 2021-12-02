1b197760e502314a47ee2bb15964bd283c7eeaaa
adeebkor <mabm4@cam.ac.uk>
2021-12-02 10:40:10 +0000

    Rotate GLL quad points and weights

--- cpp/basix/quadrature.cpp
+++ cpp/basix/quadrature.cpp
@@ -415,6 +415,9 @@ std::pair<xt::xarray<double>, std::vector<double>> compute_gll_rule(int m)
   // Compute Lobatto nodes and weights
   auto [xs_ref, ws_ref] = lobatto(alpha, beta, -1.0, 1.0);
 
+  std::rotate(xs_ref.begin()+1, xs_ref.begin()+2, xs_ref.end());
+  std::rotate(ws_ref.begin()+1, ws_ref.begin()+2, ws_ref.end());
+
   return {xt::adapt(xs_ref), ws_ref};
 }
 //-----------------------------------------------------------------------------

a6f8e09c5c3593a011c9d9c40e23d6db78327f80
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-04-28 13:20:17 +0100

    Add test of new UFL wrapper functions

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name="fenics-basix",
           "docs": ["markdown", "pylit3", "pyyaml", "sphinx", "sphinx_rtd_theme"],
           "lint": ["flake8", "pydocstyle"],
           "optional": ["numba"],
-          "test": ["pytest", "sympy", "numba", "scipy", "matplotlib"],
+          "test": ["pytest", "sympy", "numba", "scipy", "matplotlib", "fenics-ufl"],
           "ci": ["pytest-xdist", "fenics-basix[docs]", "fenics-basix[lint]", "fenics-basix[optional]",
                  "fenics-basix[test]"]
       },

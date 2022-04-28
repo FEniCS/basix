84c2ac5dfbcf480babc2a3a9c0242aa96b690839
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-04-28 13:30:20 +0100

    :

--- test/test_ufl_wrapper.py
+++ test/test_ufl_wrapper.py
@@ -9,7 +9,7 @@ import basix.ufl_wrapper
     (basix.ElementFamily.P, basix.CellType.triangle, 2),
     (basix.ElementFamily.P, "triangle", 2),
 ])
-def test_create_element(inputs)
+def test_create_element(inputs):
     basix.ufl_wrapper.create_element(*inputs)
 
 
@@ -19,7 +19,7 @@ def test_create_element(inputs)
     (basix.ElementFamily.P, basix.CellType.triangle, 2),
     (basix.ElementFamily.P, "triangle", 2),
 ])
-def test_create_vector_element(inputs)
+def test_create_vector_element(inputs):
     basix.ufl_wrapper.create_vector_element(*inputs)
 
 
@@ -29,5 +29,5 @@ def test_create_vector_element(inputs)
     (basix.ElementFamily.P, basix.CellType.triangle, 2),
     (basix.ElementFamily.P, "triangle", 2),
 ])
-def test_create_tensor_element(inputs)
+def test_create_tensor_element(inputs):
     basix.ufl_wrapper.create_tensor_element(*inputs)

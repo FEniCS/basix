ecf676a8393bfab86e577c77e68f23d769daef9f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-04-28 11:51:19 +0100

    types and _ for stuff not needed by users

--- python/basix/ufl_wrapper.py
+++ python/basix/ufl_wrapper.py
@@ -1,48 +1,54 @@
 """Functions to directly wrap Basix elements in UFL."""
 
-from ufl.finiteelement.finiteelementbase import FiniteElementBase
-import hashlib
-import basix
+from ufl.finiteelement.finiteelementbase import FiniteElementBase as _FiniteElementBase
+import hashlib as _hashlib
+import basix as _basix
 
 
-class BasixElement(FiniteElementBase):
+class BasixElement(_FiniteElementBase):
     """A wrapper allowing Basix elements to be used with UFL."""
 
-    def __init__(self, element):
+    def __init__(self, element: _basix.finite_element.FiniteElement):
         super().__init__(
             element.family.name, element.cell_type.name, element.degree, None, tuple(element.value_shape),
             tuple(element.value_shape))
 
-        if element.family == basix.ElementFamily.custom:
-            self._repr = f"custom Basix element ({compute_signature(element)})"
+        if element.family == _basix.ElementFamily.custom:
+            self._repr = f"custom Basix element ({_compute_signature(element)})"
         else:
             self._repr = (f"Basix element ({element.family.name}, {element.cell_type.name}, {element.degree}, "
                           f"{element.lagrange_variant.name}, {element.dpc_variant.name}, {element.discontinuous})")
         self.basix_element = element
 
-    def mapping(self):
-        """Get the map type for this element."""
-        return map_type_to_string(self.basix_element.map_type)
+    def mapping(self) -> str:
+        """Return the map type"""
+        return _map_type_to_string(self.basix_element.map_type)
+
+    def __eq__(self, other):
+        """Check if two elements are equal."""
+        if isinstance(other, BasixElement):
+            return self.basix_element == other.basix_element
+        return False
 
 
-def map_type_to_string(map_type):
+def _map_type_to_string(map_type: _basix.MapType) -> str:
     """Convert map type to a UFL string."""
-    if map_type == basix.MapType.identity:
+    if map_type == _basix.MapType.identity:
         return "identity"
-    if map_type == basix.MapType.L2Piola:
+    if map_type == _basix.MapType.L2Piola:
         return "L2 Piola"
-    if map_type == basix.MapType.contravariantPiola:
+    if map_type == _basix.MapType.contravariantPiola:
         return "contravariant Piola"
-    if map_type == basix.MapType.covariantPiola:
+    if map_type == _basix.MapType.covariantPiola:
         return "covariant Piola"
-    if map_type == basix.MapType.doubleContravariantPiola:
+    if map_type == _basix.MapType.doubleContravariantPiola:
         return "double contravariant Piola"
-    if map_type == basix.MapType.doubleCovariantPiola:
+    if map_type == _basix.MapType.doubleCovariantPiola:
         return "double covariant Piola"
     raise ValueError(f"Unsupported map type: {map_type}")
 
 
-def compute_signature(element):
+def _compute_signature(element: _basix.finite_element.FiniteElement):
     """Compute a signature of a custom element."""
     signature = (f"{element.cell_type.name}, {element.degree}, {element.value_shape}, {element.map_type.name}, "
                  f"{element.discontinuous}, {element.degree_bounds}, ")
@@ -61,5 +67,5 @@ def compute_signature(element):
     for mat in element.entity_transformations().values():
         data = ",".join([f"{i}" for row in mat for i in row])
         data += "__"
-    signature += hashlib.sha1(data.encode('utf-8')).hexdigest()
+    signature += _hashlib.sha1(data.encode('utf-8')).hexdigest()
     return signature

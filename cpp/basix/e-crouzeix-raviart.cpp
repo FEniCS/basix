bcf3a5232a1dea0a50071b819d25d97b17fe76f0
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-04-29 08:24:57 +0100

    Add == for custom element

--- cpp/basix/e-crouzeix-raviart.cpp
+++ cpp/basix/e-crouzeix-raviart.cpp
@@ -70,7 +70,7 @@ FiniteElement basix::element::create_cr(cell::type celltype, int degree,
     std::tie(x, M) = element::make_discontinuous(x, M, tdim, 1);
   }
 
-  return FiniteElement(element::family::CR, celltype, 1, {1},
+  return FiniteElement(element::family::CR, celltype, 1, {},
                        xt::eye<double>(ndofs), x, M, maps::type::identity,
                        discontinuous, degree, degree);
 }

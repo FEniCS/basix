7474ca99cef132221179f2fa5ae6eb098698c47f
Jack S. Hale <mail@jackhale.co.uk>
2022-05-16 13:12:58 +0200

    Tweaks

--- test/test_version.py
+++ test/test_version.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Chris Richardson and Garth Wells
+# Copyright (c) 2022 Chris Richardson, Garth Wells and Jack S. Hale
 # FEniCS Project
 # SPDX-License-Identifier: MIT
 
@@ -6,10 +6,11 @@ import pkg_resources
 import re
 import basix
 
-# From https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
-
 
 def is_canonical(version):
+    """Check version number is canonical according to PEP0440
+
+    From https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions"""
     return re.match(r'^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$', version) is not None  # noqa: E501
 
 
@@ -18,10 +19,11 @@ def test_version():
 
     assert is_canonical(version)
 
-    # Remove Python-specific versioning (dev, post) and compare with C++
+    # Strip Python-specific versioning (dev, post) and compare with C++
     # versioning
-    version = version.replace('dev', '')
-    version = re.sub(r"(\.post(0|[1-9][0-9]*))", "", version)
-    version = re.sub(r"(\.dev(0|[1-9][0-9]*))", "", version)
-    if version != basix.__version__:
-        raise RuntimeError("Incorrect installation version compared to pybind")
+    stripped_version = re.sub(r"(\.post(0|[1-9][0-9]*))", "", version)
+    stripped_version = re.sub(r"(\.dev(0|[1-9][0-9]*))", "", stripped_version)
+    if stripped_version != basix.__version__:
+        raise RuntimeError(
+            f"The version numbers of the Python (${pkg_resources.get_distribution('fenics-basix').version} "
+            + "-> ${stripped_version}) and pybind11/C++ (${basix.__version__}) libraries does not match")

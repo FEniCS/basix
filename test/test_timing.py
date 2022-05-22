f79e963027f83a1038ba0523ec02a2aa19191c12
Chris Richardson <chris@bpi.cam.ac.uk>
2022-05-22 15:40:07 +0100

    Remove file

--- test/test_timing.py
+++
@@ -1,7 +0,0 @@
-import basix
-import numpy as np
-
-
-def test_tabulate_polyset():
-    pts = [[p, p/2.0, 0.5] for p in np.linspace(0, 1, 100000)]
-    basix._basixcpp.tabulate_polynomial_set(basix.CellType.tetrahedron, 3, 0, pts)

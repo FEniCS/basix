44657d84f9cee7d88aaa0f9b260bec3ad131e2d8
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-09-26 09:18:24 +0100

    tidier method

--- python/basix/ufl_wrapper.py
+++ python/basix/ufl_wrapper.py
@@ -19,6 +19,32 @@ from ufl.algorithms.estimate_degrees import IrreducibleInt as _IrreducibleInt
 _nda_f64 = _numpy_typing.NDArray[_numpy.float64]
 
 
+_spacemap = {
+    basix.SobolevSpace.L2: ufl.sobolevspace.L2,
+    basix.SobolevSpace.H1: ufl.sobolevspace.H1,
+    basix.SobolevSpace.H2: ufl.sobolevspace.H2,
+    basix.SobolevSpace.HInf: ufl.sobolevspace.HInf,
+    basix.SobolevSpace.HDiv: ufl.sobolevspace.HDiv,
+    basix.SobolevSpace.HCurl: ufl.sobolevspace.HCurl,
+    basix.SobolevSpace.HEin: ufl.sobolevspace.HEin,
+    basix.SobolevSpace.HDivDiv: ufl.sobolevspace.HDivDiv,
+}
+
+
+def ufl_sobolev_space_from_enum(s: basix.SobolevSpace):
+    """Convert a Basix Sobolev space enum to a UFL Sobolev space.
+
+    Args:
+        s: The Basix Sobolev space
+
+    Returns:
+        UFL Sobolev space
+    """
+    if s not in _spacemap:
+        raise ValueError(f"Could not convert to UFL Sobolev space: {s.name}")
+    return _spacemap[s]
+
+
 class _BasixElementBase(_FiniteElementBase):
     """A base wrapper to allow Basix elements to be used with UFL.
 
@@ -275,8 +301,7 @@ class _BasixElementBase(_FiniteElementBase):
 
     def sobolev_space(self):
         """Return the underlying Sobolev space."""
-        return 
-getattr(_ufl.sobolevspace, self.basix_sobolev_space.name)
+        return ufl_sobolev_space_from_enum(self.basix_sobolev_space)
 
     @property
     @_abstractmethod

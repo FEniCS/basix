9fb8b207cd36b808dcff3ee4bc6f4818fc771b9a
Susanne Claus <Susanne.Claus@ucl.ac.uk>
2022-10-06 03:50:38 +0200

    C wrapper for tabulate tensor function of basix

---
+++ cpp/basix/cwrapper.cpp
@@ -0,0 +1,79 @@
+// Copyright (c) 2022 Susanne Claus
+// FEniCS Project
+// SPDX-License-Identifier:    MIT
+
+// This file defines the C-API functions listed in cwrapper.h 
+// This is a C++ file that defines C calls 
+#include <basix/finite-element.h>
+#include <basix/cell.h>
+#include <span>
+#include "cwrapper.h"
+
+extern "C" {
+
+basix_element* basix_element_create(const int basix_family, const int basix_cell_type, const int degree, 
+                                    const int lagrange_variant, const int dpc_variant, const bool discontinuous, 
+                                    const unsigned int gdim)
+{
+    basix_element* element = (basix_element*) malloc(sizeof(basix_element));
+    element->basix_family = basix_family; 
+    element->basix_cell_type = basix_cell_type;
+    element->degree = degree;
+    element->lagrange_variant = lagrange_variant; 
+    element->dpc_variant = dpc_variant; 
+    element->discontinuous = discontinuous; 
+    element->gdim = gdim; 
+
+    return element; 
+}
+
+void basix_element_destroy(basix_element *element)
+{
+    free(element);
+}
+
+void basix_element_tabulate_shape(const basix_element *element, const unsigned int num_points, 
+                                  const int nd, int* cshape)
+{
+    //Specify which element is needed
+    basix::element::family family = static_cast<basix::element::family>(element->basix_family); 
+    basix::cell::type cell_type = static_cast<basix::cell::type>(element->basix_cell_type); 
+    int k = element->degree;
+    basix::element::lagrange_variant lvariant = static_cast<basix::element::lagrange_variant>(element->lagrange_variant);
+    basix::element::dpc_variant dvariant = static_cast<basix::element::dpc_variant>(element->dpc_variant);
+
+    std::size_t gdim = element->gdim;
+
+    //Create C++ basix element object
+    basix::FiniteElement finite_element = basix::create_element(family, cell_type, k, lvariant,dvariant,element->discontinuous);
+
+    //Determine shape of tabulated values to allocate sufficient memory 
+    std::array<std::size_t, 4> shape = finite_element.tabulate_shape(nd, num_points);
+
+    //Copy shape values 
+    std::copy(shape.begin(),shape.end(),cshape);
+}
+
+void basix_element_tabulate(const basix_element *element, const double* points,
+                            const unsigned int num_points, const int nd, 
+                            double* table_data, long unsigned int table_data_size)
+{
+     //Specify which element is needed
+    basix::element::family family = static_cast<basix::element::family>(element->basix_family); 
+    basix::cell::type cell_type = static_cast<basix::cell::type>(element->basix_cell_type); 
+    int k = element->degree;
+    std::size_t gdim = element->gdim;
+    basix::element::lagrange_variant lvariant = static_cast<basix::element::lagrange_variant>(element->lagrange_variant);
+
+    //Create C++ basix element object
+    basix::FiniteElement finite_element = basix::create_element(family, cell_type, k, lvariant);
+
+    //Create span views on points and table values
+    std::span<const double> points_view{points,num_points*gdim};
+    std::span<double> basis{table_data, table_data_size};
+    std::array<std::size_t, 2> xshape{num_points,gdim};
+    
+    finite_element.tabulate(nd, points_view, xshape, basis);
+}
+
+}
---
+++ cpp/basix/cwrapper.h
@@ -0,0 +1,58 @@
+// Copyright (c) 2022 Susanne Claus
+// FEniCS Project
+// SPDX-License-Identifier:    MIT
+
+// Declares a simple C API wrapper around C++.
+
+#ifndef BASIX_CWRAPPER_H_
+#define BASIX_CWRAPPER_H_
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+// extern C indicates to C++ compiler that the following code 
+// is meant to be called by a C compiler 
+// i.e. tells the compiler that C naming conventions etc should be used
+extern "C" {
+#endif
+
+//@todo Could this be replaced by ufcx_finite_element? 
+// The following int values indicate the position of the values in the corresponding 
+// enum classes in basix
+typedef struct basix_element
+{    
+  /// Basix identifier of the family 
+  int basix_family;
+  /// Basix identifier of the cell type
+  int basix_cell_type;
+  /// Polynomial degree 
+  int degree;
+  /// The Lagrange variant to be passed to Basix's create_element function
+  int lagrange_variant;
+  /// The DPC variant to be passed to Basix's create_element function
+  int dpc_variant;
+  /// Indicates whether or not this is the discontinuous version of the element
+  bool discontinuous;
+  //The geometric dimension
+  int gdim;
+} basix_element;
+
+basix_element* basix_element_create(const int basix_family, const int basix_cell_type, const int degree, 
+                                    const int lagrange_variant, const int dpc_variant, const bool discontinuous, 
+                                    const unsigned int gdim);
+
+void basix_element_destroy(basix_element *element);
+
+void basix_element_tabulate(const basix_element *element, const double* points,
+                            const unsigned int num_points, const int nd, 
+                            double* table_data, long unsigned int table_data_size);
+
+void basix_element_tabulate_shape(const basix_element *element, const unsigned int num_points, 
+                                  const int nd, int* cshape);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* BASIX_CWRAPPER_H_ */

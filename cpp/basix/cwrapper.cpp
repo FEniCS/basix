a851034288b8ea3059d286ccdeaff8ed3f636c24
Susanne Claus <Susanne.Claus@ucl.ac.uk>
2022-10-21 01:48:06 +0200

    change to create_element C wrapper

--- cpp/basix/cwrapper.cpp
+++ cpp/basix/cwrapper.cpp
@@ -12,68 +12,50 @@
 extern "C" {
 
 basix_element* basix_element_create(const int basix_family, const int basix_cell_type, const int degree, 
-                                    const int lagrange_variant, const int dpc_variant, const bool discontinuous, 
-                                    const unsigned int gdim)
+                                    const int lagrange_variant, const int dpc_variant, const bool discontinuous)
 {
-    basix_element* element = (basix_element*) malloc(sizeof(basix_element));
-    element->basix_family = basix_family; 
-    element->basix_cell_type = basix_cell_type;
-    element->degree = degree;
-    element->lagrange_variant = lagrange_variant; 
-    element->dpc_variant = dpc_variant; 
-    element->discontinuous = discontinuous; 
-    element->gdim = gdim; 
+    //Specify which element is needed
+    basix::element::family family = static_cast<basix::element::family>(basix_family); 
+    basix::cell::type cell_type = static_cast<basix::cell::type>(basix_cell_type); 
+    int k = degree;
+    basix::element::lagrange_variant lvariant = static_cast<basix::element::lagrange_variant>(lagrange_variant);
+    basix::element::dpc_variant dvariant = static_cast<basix::element::dpc_variant>(dpc_variant);
+
+    //Create C++ basix element object
+    basix::FiniteElement finite_element = basix::create_element(family, cell_type, k, lvariant,dvariant,discontinuous);
+
+    basix_element* element = reinterpret_cast<basix_element*>(new basix::FiniteElement(finite_element));
 
     return element; 
 }
 
 void basix_element_destroy(basix_element *element)
 {
-    free(element);
+    delete reinterpret_cast<basix::FiniteElement*>(element);
 }
 
-void basix_element_tabulate_shape(const basix_element *element, const unsigned int num_points, 
+void basix_element_tabulate_shape(basix_element *element, const unsigned int num_points, 
                                   const int nd, int* cshape)
 {
-    //Specify which element is needed
-    basix::element::family family = static_cast<basix::element::family>(element->basix_family); 
-    basix::cell::type cell_type = static_cast<basix::cell::type>(element->basix_cell_type); 
-    int k = element->degree;
-    basix::element::lagrange_variant lvariant = static_cast<basix::element::lagrange_variant>(element->lagrange_variant);
-    basix::element::dpc_variant dvariant = static_cast<basix::element::dpc_variant>(element->dpc_variant);
-
-    //Create C++ basix element object
-    basix::FiniteElement finite_element = basix::create_element(family, cell_type, k, lvariant,dvariant,element->discontinuous);
-
     //Determine shape of tabulated values to allocate sufficient memory 
-    std::array<std::size_t, 4> shape = finite_element.tabulate_shape(nd, num_points);
+    std::array<std::size_t, 4> shape = reinterpret_cast<basix::FiniteElement*>(element)->tabulate_shape(nd, num_points);
 
     //Copy shape values 
     std::copy(shape.begin(),shape.end(),cshape);
 }
 
-void basix_element_tabulate(const basix_element *element, const double* points,
+void basix_element_tabulate(basix_element *element, const unsigned int gdim, const double* points,
                             const unsigned int num_points, const int nd, 
                             double* table_data, long unsigned int table_data_size)
 {
-    //Specify which element is needed
-    basix::element::family family = static_cast<basix::element::family>(element->basix_family); 
-    basix::cell::type cell_type = static_cast<basix::cell::type>(element->basix_cell_type); 
-    int k = element->degree;
-    basix::element::lagrange_variant lvariant = static_cast<basix::element::lagrange_variant>(element->lagrange_variant);
-    basix::element::dpc_variant dvariant = static_cast<basix::element::dpc_variant>(element->dpc_variant);
-
-    std::size_t gdim = element->gdim;
-
-    //Create C++ basix element object
-    basix::FiniteElement finite_element = basix::create_element(family, cell_type, k, lvariant,dvariant,element->discontinuous);
+    basix::FiniteElement* finite_element = reinterpret_cast<basix::FiniteElement*>(element);
 
     //Create span views on points and table values
     std::span<const double> points_view{points,num_points*gdim};
     std::span<double> basis{table_data, table_data_size};
     std::array<std::size_t, 2> xshape{num_points,gdim};
     
-    finite_element.tabulate(nd, points_view, xshape, basis);
+    finite_element->tabulate(nd, points_view, xshape, basis);
 }
 
 }
--- cpp/basix/cwrapper.h
+++ cpp/basix/cwrapper.h
@@ -20,36 +20,20 @@ extern "C" {
 /// Basix element structure to map c basix element to C++ basix finite_element
 /// The following int values indicate the position of the values in the corresponding 
 /// enum classes in basix
-typedef struct basix_element
-{    
-  /// Basix identifier of the family 
-  int basix_family;
-  /// Basix identifier of the cell type
-  int basix_cell_type;
-  /// Polynomial degree 
-  int degree;
-  /// The Lagrange variant to be passed to Basix's create_element function
-  int lagrange_variant;
-  /// The DPC variant to be passed to Basix's create_element function
-  int dpc_variant;
-  /// Indicates whether or not this is the discontinuous version of the element
-  bool discontinuous;
-  /// The geometric dimension
-  int gdim;
-} basix_element;
+typedef struct basix_element basix_element;
 
 basix_element* basix_element_create(const int basix_family, const int basix_cell_type, const int degree, 
-                                    const int lagrange_variant, const int dpc_variant, const bool discontinuous, 
-                                    const unsigned int gdim);
+                                    const int lagrange_variant, const int dpc_variant, const bool discontinuous);
 
 void basix_element_destroy(basix_element *element);
 
-void basix_element_tabulate(const basix_element *element, const double* points,
+void basix_element_tabulate_shape(basix_element *element, const unsigned int num_points, 
+                                  const int nd, int* cshape);
+
+void basix_element_tabulate(basix_element *element, const unsigned int gdim, const double* points,
                             const unsigned int num_points, const int nd, 
                             double* table_data, long unsigned int table_data_size);
 
-void basix_element_tabulate_shape(const basix_element *element, const unsigned int num_points, 
-                                  const int nd, int* cshape);
 
 #ifdef __cplusplus
 }

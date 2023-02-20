10611864d8698c7756524595941d8c979384e764
Garth N. Wells <gnw20@cam.ac.uk>
2023-02-20 15:43:49 +0000

    Remove deprecated package

--- python/setup.py
+++ python/setup.py
@@ -85,7 +85,7 @@ setup(name='fenics-basix',
       package_data={"basix": ["py.typed"]},
       install_requires=["numpy>=1.21"],
       extras_require={
-          "docs": ["markdown", "pylit3", "pyyaml", "sphinx==5.0.2", "sphinx_rtd_theme"],
+          "docs": ["markdown", "pyyaml", "sphinx==5.0.2", "sphinx_rtd_theme"],
           "lint": ["flake8", "pydocstyle"],
           "optional": ["numba", "fenics-ufl@git+https://github.com/fenics/ufl"],
           "test": ["pytest", "sympy", "scipy", "matplotlib", "fenics-basix[optional]"],

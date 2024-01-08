75f7d4e08d68cd6b2e2019a4c75755fa76c6467f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-01-08 14:59:17 +0000

    add test

--- test/test_equality.py
+++ test/test_equality.py
@@ -8,12 +8,14 @@ import basix
 
 
 def test_element_equality():
-    p1 = basix.create_element(basix.ElementFamily.P, basix.CellType.triangle, 1)
-    p1_again = basix.create_element(basix.ElementFamily.P, basix.CellType.triangle, 1)
+    p1 = basix.create_element(basix.ElementFamily.P, basix.CellType.triangle, 1, dtype=np.float64)
+    p1_again = basix.create_element(basix.ElementFamily.P, basix.CellType.triangle, 1, dtype=np.float64)
     rt1 = basix.create_element(basix.ElementFamily.RT, basix.CellType.triangle, 1)
     p1_quad = basix.create_element(basix.ElementFamily.P, basix.CellType.quadrilateral, 1)
     p4_gll = basix.create_element(basix.ElementFamily.P, basix.CellType.triangle, 4, basix.LagrangeVariant.gll_warped)
     p4_equi = basix.create_element(basix.ElementFamily.P, basix.CellType.triangle, 4, basix.LagrangeVariant.equispaced)
+
+    p1_f32 = basix.create_element(basix.ElementFamily.P, basix.CellType.triangle, 1, dtype=np.float32)
 
     assert p1 == p1
     assert p1 == p1_again
@@ -22,6 +24,8 @@ def test_element_equality():
     assert p4_gll != p4_equi
     assert p1 != p1_quad
     assert p1 != rt1
+
+    assert p1 == p1_f32
 
 
 def test_custom_element_equality():

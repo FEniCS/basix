902a8c39293da4770f419c3351504e503479e345
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-02-02 15:45:25 +0000

    Add create_tp_element function (#780)
    * Add make_tp_element
    
    * fix
    
    * create_tp_element
    
    * add other tp functions to python wrapper
    
    * isort
    
    * add wrap_element to __all__
    
    * ffc branch
    
    * update pyi
    
    * list[list[]]
    
    * templating
    
    * throw rather than returning empty list
    
    * doc tweak
    
    * RuntimeError
    
    * doc
    
    ---------
    
    Co-authored-by: Chris Richardson <chris@bpi.cam.ac.uk>

--- python/basix/ufl.py
+++ python/basix/ufl.py
@@ -22,7 +22,7 @@ from ufl.pullback import UndefinedPullback as _UndefinedPullback
 import basix as _basix
 
 __all__ = ["element", "enriched_element", "custom_element", "mixed_element",
-           "quadrature_element", "real_element", "blocked_element"]
+           "quadrature_element", "real_element", "blocked_element", "wrap_element"]
 
 _spacemap = {
     _basix.SobolevSpace.L2: _ufl.sobolevspace.L2,
@@ -1175,7 +1175,7 @@ class _BlockedElement(_ElementBase):
             # Repeat sub element horizontally
             assert len(table.shape) == 2
             new_table = np.zeros((table.shape[0], *self._block_shape,
-                                   self._block_size * table.shape[1]))
+                                  self._block_size * table.shape[1]))
             for i, j in enumerate(_itertools.product(*[range(s) for s in self._block_shape])):
                 if len(j) == 1:
                     new_table[:, j[0], i::self._block_size] = table
@@ -1375,7 +1375,7 @@ class _BlockedElement(_ElementBase):
             M_row = []
             for mat in M_list:
                 new_mat = np.zeros((mat.shape[0] * self._block_size, mat.shape[1]
-                                     * self._block_size, mat.shape[2], mat.shape[3]))
+                                    * self._block_size, mat.shape[2], mat.shape[3]))
                 for i in range(self._block_size):
                     new_mat[i * mat.shape[0]: (i + 1) * mat.shape[0],
                             i * mat.shape[1]: (i + 1) * mat.shape[1], :, :] = mat

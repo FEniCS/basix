f1390de608adeceb830edc30fdd6ed965a145687
Jack S. Hale <mail@jackhale.co.uk>
2024-04-30 10:58:18 +0200

    Fix for win32.

--- demo/cpp/test.py
+++ demo/cpp/test.py
@@ -2,6 +2,7 @@
 
 import os
 from subprocess import run
+import sys
 
 import pytest
 
@@ -25,6 +26,13 @@ def test_demo(demo, cmake_args):
     """Test demos."""
     demo_source = os.path.join(path, demo)
     demo_build = os.path.join(path, demo, "_build")
+
     run(f"cmake {cmake_args} -B {demo_build} -S {demo_source}", check=True, shell=True)
     run(f"cmake --build {demo_build}", check=True, shell=True)
-    run(os.path.join(demo_build, demo), check=True, shell=True)
+
+    if sys.platform.startswith("win32"):
+        demo_executable = demo + ".exe"
+    else:
+        demo_executable = demo
+
+    run(os.path.join(demo_build, demo_executable), check=True, shell=True)

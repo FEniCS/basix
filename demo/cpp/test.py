05872458fc9ab880e7693722afa066646a0e4881
Jack S. Hale <mail@jackhale.co.uk>
2024-04-30 10:09:58 +0200

    Fix.

--- demo/cpp/test.py
+++ demo/cpp/test.py
@@ -23,8 +23,8 @@ def cmake_args(request):
 @pytest.mark.parametrize("demo", demos)
 def test_demo(demo, cmake_args):
     """Test demos."""
+    demo_source = os.path.join(path, demo)
     demo_build = os.path.join(path, demo, "_build")
-    run(f"cmake {cmake_args} -B {demo_build} -S {demo}", check=True, capture_output=True)
+    run(f"cmake {cmake_args} -B {demo_build} -S {demo_source}", check=True, capture_output=True)
     run(f"cmake --build {demo_build}", check=True)
-    run(f"cmake --install {demo_build}", check=True)
     run(os.path.join(demo_build, demo), check=True)

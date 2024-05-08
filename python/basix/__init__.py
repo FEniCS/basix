71b5db3436d55bbb03d346146d9c02637c7cfdc4
Jack S. Hale <mail@jackhale.co.uk>
2024-05-08 11:38:28 +0200

    Fix.

--- python/basix/__init__.py
+++ python/basix/__init__.py
@@ -9,7 +9,7 @@ The core of the library is written in C++, but the majority of Basix's
 functionality can be used via this Python interface.
 """
 import os
-os.add_dll_directory('D:/a/basix/install/bin')
+os.add_dll_directory('D:/a/dolfinx/basix-install/bin')
 
 from basix import cell, finite_element, lattice, polynomials, quadrature, sobolev_spaces
 from basix._basixcpp import __version__

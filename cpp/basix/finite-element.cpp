1868dd3c4e4266bc1b017b814f2837de4d07e863
Garth N. Wells <gnw20@cam.ac.uk>
2024-11-15 18:46:31 +0000

    Make eps consistent

--- cpp/basix/finite-element.cpp
+++ cpp/basix/finite-element.cpp
@@ -1436,7 +1436,7 @@ FiniteElement<F>::FiniteElement(
     for (std::size_t col = 0; col < matM.extent(1); ++col)
     {
       F v = col == row ? 1.0 : 0.0;
-      constexpr F eps = 100 * std::numeric_limits<F>::epsilon();
+      const F eps = 10 * degree * degree * std::numeric_limits<F>::epsilon();
       if (std::abs(matM(row, col) - v) > eps)
       {
         _interpolation_is_identity = false;

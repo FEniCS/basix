53c73a57b5bb6f9c20a3bd81a125ab74c22fc12f
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-03-02 20:34:15 +0100

    Introduce namespace aliases

---
+++ cpp/basix/types.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2025 Paul T. Kühner
+//
+// This file is part of DOLFINx (https://www.fenicsproject.org)
+//
+// SPDX-License-Identifier:    LGPL-3.0-or-later
+
+#include "mdspan.hpp"
+
+namespace basix
+{
+/// @private mdspan namespace
+namespace md = MDSPAN_IMPL_STANDARD_NAMESPACE;
+
+/// @private mdarray namespace
+namespace mdex = MDSPAN_IMPL_STANDARD_NAMESPACE::MDSPAN_IMPL_PROPOSED_NAMESPACE;
+} // namespace basix

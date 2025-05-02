3148286b4f238d4c08ead9b5e8d6b085bdc9badc
samuel0007 <russo.samuel20@gmail.com>
2025-05-02 15:47:27 +0000

    format with ruff

--- test/test_dof_ordering.py
+++ test/test_dof_ordering.py
@@ -3,11 +3,12 @@
 # SPDX-License-Identifier: MIT
 
 import numpy as np
-
-import basix
 import pytest
 
+import basix
+
 np.set_printoptions(suppress=True)
+
 
 @pytest.mark.parametrize(
     "lagrange_variant",
@@ -15,12 +16,12 @@ np.set_printoptions(suppress=True)
         basix.LagrangeVariant.gll_warped,
         basix.LagrangeVariant.legendre,
         basix.LagrangeVariant.bernstein,
-        basix.LagrangeVariant.chebyshev_warped
+        basix.LagrangeVariant.chebyshev_warped,
     ],
 )
 def test_ordering(lagrange_variant):
     pt = np.array([[1 / 3, 1 / 3], [0.3, 0.2]])
-    
+
     is_dc = lagrange_variant in [
         basix.LagrangeVariant.legendre,
         basix.LagrangeVariant.chebyshev_warped,
@@ -28,16 +29,26 @@ def test_ordering(lagrange_variant):
 
     # reordered element
     el = basix.create_element(
-        basix.ElementFamily.P, basix.CellType.triangle, 2, lagrange_variant, basix.DPCVariant.unset, 
-        is_dc, dof_ordering=[0, 3, 5, 1, 2, 4]
+        basix.ElementFamily.P,
+        basix.CellType.triangle,
+        2,
+        lagrange_variant,
+        basix.DPCVariant.unset,
+        is_dc,
+        dof_ordering=[0, 3, 5, 1, 2, 4],
     )
     order = el.dof_ordering
     result1 = el.tabulate(1, pt)
 
     # standard element
     el = basix.create_element(
-        basix.ElementFamily.P, basix.CellType.triangle, 2, lagrange_variant, basix.DPCVariant.unset, 
-        is_dc)
+        basix.ElementFamily.P,
+        basix.CellType.triangle,
+        2,
+        lagrange_variant,
+        basix.DPCVariant.unset,
+        is_dc,
+    )
     result2 = el.tabulate(1, pt)
     presult = np.zeros_like(result2)
 

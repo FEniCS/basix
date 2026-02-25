ce8313cc6ed673af5170839702bf29dafe9166ea
Jack S. Hale <mail@jackhale.co.uk>
2026-02-25 15:45:56 +0100

    Revert - mysterious how it works in CI and not locally

--- python/basix/ufl.py
+++ python/basix/ufl.py
@@ -358,12 +358,12 @@ class _ElementBase(_AbstractFiniteElement):
         raise NotImplementedError()
 
     @property
-    def _x(self) -> list[list[_npt.ArrayLike]]:
+    def _x(self) -> list[list[_npt.NDArray]]:
         """The points used to define interpolation."""
         raise NotImplementedError()
 
     @property
-    def _M(self) -> list[list[_npt.ArrayLike]]:
+    def _M(self) -> list[list[_npt.NDArray]]:
         """The matrices used to define interpolation."""
         raise NotImplementedError()
 
@@ -555,11 +555,11 @@ class _BasixElement(_ElementBase):
         return self._element.wcoeffs
 
     @property
-    def _x(self) -> list[list[_npt.ArrayLike]]:
+    def _x(self) -> list[list[_npt.NDArray]]:
         return self._element.x
 
     @property
-    def _M(self) -> list[list[_npt.ArrayLike]]:
+    def _M(self) -> list[list[_npt.NDArray]]:
         return self._element.M
 
     @property
@@ -1182,11 +1182,11 @@ class _BlockedElement(_ElementBase):
         return wcoeffs
 
     @property
-    def _x(self) -> list[list[_npt.ArrayLike]]:
+    def _x(self) -> list[list[_npt.NDArray]]:
         return self._sub_element._x
 
     @property
-    def _M(self) -> list[list[_npt.ArrayLike]]:
+    def _M(self) -> list[list[_npt.NDArray]]:
         M = []
         for M_list in self._sub_element._M:
             M_row = []
